51a77de4f703c5195ac55b751d5c4e152c1e3ba4
venkatkanagavalli <venkatkanagavalli.d94@gmail.com>
2026-08-08 22:28:26 +0530

    Update index.js
    sfsf

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function startServer() {
     const PORT = process.env.PORT || 3000;
 
     app.listen(PORT, () => {
-        console.log(`Application running on port ${PORT}`);
+        console.log(`Application  running on port ${PORT}`);
     });
 }
 
