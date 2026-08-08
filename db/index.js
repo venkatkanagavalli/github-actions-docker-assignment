e7ddf75bda3203f7016fe3f46b55423a2a339b6e
venkatkanagavalli <venkatkanagavalli.d94@gmail.com>
2026-08-08 20:55:38 +0530

    Update index.js
    test

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ async function connectDB() {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
 
-        console.log("MongoDB Connected");
+        console.log("MongoDB Connected ");
     } catch (error) {
         console.error("MongoDB connection failed: ", error.message);
         process.exit(1);
